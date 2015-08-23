ea52ef32dbb0c2c922fbc124b6ec463cd53f3695
bski006 <boch@umich.edu>
2015-08-23 01:41:50 -0700

    dep

--- ra168e/spiders/ra168e_spider.py
+++ ra168e/spiders/ra168e_spider.py
@@ -11,7 +11,7 @@ class images (scrapy.Spider):
 		self.url_file = file_name
 	def parse(self, response):
 		images_source = pickle.load (open(self.images_urls_path + self.url_file, "rb"))
-		for x, y in images_source.items()[3]:
+		for x, y in images_source.items():
 			print x, len(y)
 			car_images = image_doc()
 			car_images["label"] = x
